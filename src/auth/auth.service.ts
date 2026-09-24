9997a0fa5368592611f1aad387977e6c3e53c09e
maejinstro <maejinstro@gmail.com>
2026-09-24 16:22:45 +0200

    bcrypt added. password hashed.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,20 +2,34 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service.js';
 import { RegisterAuthDto } from './dto/register-auth.dto.js';
 import { LoginAuthDto } from './dto/login-auth.dto.js';
+import bcrypt from 'bcrypt';
+
+
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
-  register(registerAuthDto: RegisterAuthDto) {
+  async register(registerAuthDto: RegisterAuthDto) {
+
+    registerAuthDto.password = await bcrypt.hash(registerAuthDto.password, 10)
+    
     return this.userService.create(registerAuthDto);
   }
 
+
   async login(loginAuthDto: LoginAuthDto) {
     const user = await this.userService.findByEmail(loginAuthDto.email);
-    if (!user || user.password !== loginAuthDto.password) {
+
+
+    if (!user) {
       throw new UnauthorizedException('Email ou mot de passe incorrect');
     }
-    return user;
+
+    if (await bcrypt.compare(loginAuthDto.password, user.password)) {
+      throw new UnauthorizedException('Email ou mot de passe incorrect');
+    }
+
+    //appel token
   }
 }
