c4bf1b4c611a71cbaa78855b611e8330e601b219
maejinstro <93771633+maejinstro@users.noreply.github.com>
2026-09-25 13:45:53 +0200

    Revert "feat(support-message): add model and service for the contact form"

--- src/support-message/dto/create-support-message.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateSupportMessageDto {
-  @ApiProperty({ example: 'Problème de connexion' })
-  subject: string;
-
-  @ApiProperty({ example: "Je n'arrive pas à me connecter à mon compte." })
-  message: string;
-}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,5 @@
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, type Relation } from 'typeorm';
 import { Item } from '../../item/entities/item.entity.js';
-import { SupportMessage } from '../../support-message/entities/support-message.entity.js';
 
 @Entity()
 export class User {
@@ -24,7 +23,4 @@ export class User {
 
   @OneToMany(() => Item, (item) => item.user)
   items: Relation<Item[]>;
-
-  @OneToMany(() => SupportMessage, (supportMessage) => supportMessage.user)
-  supportMessages: Relation<SupportMessage[]>;
 }
