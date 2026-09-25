c07f2d04c6e05d555004214cc0d2cf60645c5c00
Chrtsophe Chanteux <chanteux.titof@gmail.com>
2026-09-25 11:37:52 +0200

    Add env example and modify app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { UserModule } from './user/user.module.js';
@@ -8,21 +10,29 @@ import { ItemModule } from './item/item.module.js';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: '',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
+
     UserModule,
     AuthModule,
     ItemModule,
   ],
+
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
