1df1cf5a5198c85f8a44feee9b0c6533e6d78023
soreangsey <kivsoreangsey@gmail.com>
2026-09-25 11:43:22 +0200

    add nest/swagger modified login register create item and update item

--- src/auth/dto/register-auth.dto.ts
+++ src/auth/dto/register-auth.dto.ts
@@ -1,5 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RegisterAuthDto {
+  @ApiProperty({
+    example: 'john',
+  })
   username: string;
+
+  @ApiProperty({
+    example: 'john@example.com',
+  })
   email: string;
+
+  @ApiProperty({
+    example: 'password123',
+  })
   password: string;
 }
--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,6 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateItemDto {
-    title : string;
-    description : string;
-    imageUrl : string;
-    userId : number;
+  @ApiProperty({
+    example: 'Laptop',
+  })
+  title: string;
+
+  @ApiProperty({
+    example: 'Dell Latitude laptop',
+  })
+  description: string;
+
+  @ApiProperty({
+    example: 'https://example.com/laptop.jpg',
+  })
+  imageUrl: string;
 }
